b2dfad0daa4ffd417f9496f0a7d193664665a073
Vishal772-pixel <fantasydark001@gmail.com>
2025-07-14 15:49:35 +0530

    Test co-authored commit
    Co-authored-by: Vishal Tiwari <vishal.urban.wheels@gmail.com>

---
+++ Backend/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import express from "express";
+
+
+
+const connectDb = async()=>{
+    try{
+        await mongoose.connect(process.env.MONGODB_URI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true, 
+
+            useCreateIndex: true
+        });
+        console.log("MongoDB connected successfully");
+    }catch(error){
+        console.error("MongoDB connection failed:", error.message);
+        process.exit(1); // Exit the process with failure
+        
+    }
+}
