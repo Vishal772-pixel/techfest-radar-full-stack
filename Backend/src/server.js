02bf334c0ffa4ec1cb5878ddd7bcd59e14a8f057
Vishal772-pixel <fantasydark001@gmail.com>
2025-07-14 15:53:45 +0530

    Test co-authored commit
    Co-authored-by: Vishal Tiwari <vishal.urban.wheels@gmail.com>

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
 connectDb();
 
+dotenv.config();
 
+
+app.get('/',(req,res)=>{res.send("Server is running successfully!")});
 
 
         
